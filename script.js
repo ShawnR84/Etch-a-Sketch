0bfd13c40bda6b2a59264477ebc787f1cc457dd2
ShawnR84 <shawnrodarte@gmail.com>
2025-04-09 19:24:06 -0700

    add button

---
+++ script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('#content');
+
+const buttonDiv = document.querySelector('#top');
+
+const button = document.createElement('button');
+button.textContent = "Generate";
+button.setAttribute("id", "button")
+
+
+
+function sketchPad (element, quantity, parentElement) {
+    const value = quantity * quantity;
+    for(let i = 0; i < value; i++) {
+        const square = document.createElement(element);
+        parentElement.appendChild(square);
+        square.setAttribute("class", "square");
+    }
+}
+
+buttonDiv.appendChild(button);
+
+sketchPad("div", 100, container);
+
+
+document.addEventListener("mouseover", function(event) {
+    if(event.target.classList.contains("square")){
+        event.target.classList.add("blue");
+    }
+});
+
+function emptyDom() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    
+}
+
+button.addEventListener("click", () => {
+    let value = prompt("How many squares for new grid?");
+     if(value <= 100) {
+        emptyDom()
+        sketchPad("div", value, container);
+}
+});
